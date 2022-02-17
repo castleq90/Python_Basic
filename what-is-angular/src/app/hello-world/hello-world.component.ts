1e7d983031f652407e845972a3ceb9cd521f66f6
castleq90 <castleq37@gmail.com>
2022-02-17 18:09:11 +0900

    study

---
+++ what-is-angular/src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,13 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hello-world',
+  template: `
+    <h2>Hello World</h2>
+    <p>This is my first component!</p>
+  `
+})
+export class HelloWorldComponent {
+  // 여기에는 컴포넌트의 동작을 정의하는 코드가 들어갑니다.
+}
